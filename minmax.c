3e5408c709ba9ec5188ba817250178acd8f01c24
Rsakhuja <36058972+Rsakhuja@users.noreply.github.com>
2019-03-07 00:13:57 -0500

    Add files via upload

--- minmax.c
+++ minmax.c
@@ -21,6 +21,41 @@ int* loadFile(FILE *myFile){
   return arr;
 }
 
+//Find min,max,sum
+int* findValues(int *arr){
+  int values[3] = {0,0,0};
+  int min = arr[0];
+  int max = arr[0];
+  int sum = 0;
+  int arrSize;
+
+  arrSize = sizeof(arr)/sizeof(int);
+  int i;
+
+  for(i = 0; i< arrSize; i = i+1){
+    printf("%d\n",arr[i]);
+    if(min > arr[i]){
+      min = arr[i];
+      values[0] = min;
+      printf("Min: %d\n", values[0]);
+    }
+    if(max < arr[i]){
+      max = arr[i];
+      values[1] = max;
+      printf("Max: %d\n", values[1]);
+    }
+    sum = sum + arr[i];
+    values[2] = sum;
+    printf("Sum: %d\n", values[2]);
+  }
+
+  // printf("\nMinimum is %d\n", min);
+  // printf("\nMaximum is %d\n", max);
+  // printf("\n Sum is %d\n", sum);
+
+  return values;
+}
+
 
 int main(int argc, char *argv[]){
   FILE *myFile;
@@ -29,9 +64,11 @@ int main(int argc, char *argv[]){
   myFile = fopen(path,"r");
   int *numArray = loadFile(myFile);
 
+  int *min = findValues(numArray);
+
   int index = 0;
-  for(index = 0; index < 8; index++){
-    printf("%d\n",numArray[index]);
+  for(index = 0; index < 3; index = index + 1){
+    printf("%d\n", min[index]);
   }
 
 
